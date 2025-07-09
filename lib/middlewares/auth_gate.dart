616826971fb73c893ae5b957ef25919852434327
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-07-09 22:00:14 +0400

    feat(middleware): Add auth middleware for redirection to homepage on authed user.

---
+++ lib/middlewares/auth_gate.dart
@@ -0,0 +1,28 @@
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_amplify_auth_starter/features/auth/login/login_page.dart';
+import 'package:flutter_amplify_auth_starter/features/home/home_page.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<AuthSession>(
+      future: Amplify.Auth.fetchAuthSession(),
+      builder: (ctx, snap) {
+        if (snap.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+        
+        if (snap.hasError || !(snap.data?.isSignedIn ?? false)) {
+          return const LoginPage();
+        }
+
+        return const HomePage();
+      },
+    );
+  }
+}
