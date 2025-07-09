43761be51420a5b8cfc98335dc8630b52b39c682
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-07-09 21:24:10 +0400

    feat(theme): Add colors.

---
+++ lib/theme/dark_colors.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class DarkColors {
+  static const backgroundBody = Color(0xFF1D1B30);
+  static const backgroundBodyRgb = Color.fromRGBO(29, 27, 48, 1);
+  static const backgroundCard1 = Color(0xFF242236);
+  static const backgroundCard2 = Color(0xFF2A283F);
+  static const backgroundTag = Color(0xFF413D5C);
+  static const backgroundInput1 = Color(0xFF302D45);
+  static const backgroundInput2 = Color(0xFF29273E);
+  static const backgroundModal = Color(0xFF201E34);
+  static const backgroundBtnPrimary = Color(0xFFFDC37F);
+  static const backgroundBtnSecondary = Color(0xFF413D5C);
+  static const backgroundBtnGreen = Color(0xFF3ABD83);
+  static const backgroundBtnRed = Color(0xFFC95241);
+  static const backgroundBtnPurple = Color(0xFF774DDD);
+
+  static const backgroundBtnPrimaryHover = Color(0xFFD49851);
+  static const backgroundBtnSecondaryHover = Color(0xFF322F4B);
+  static const backgroundBtnGreenHover = Color(0xFF219763);
+  static const backgroundBtnRedHover = Color(0xFFA13526);
+  static const backgroundBtnPurpleHover = Color(0xFF5D32C7);
+  static const inputHover = Color(0xFF2D2B44);
+  static const textMenuHover = Color(0xFF29263D);
+
+  static const brandOrange = Color(0xFFFDCF7F);
+  static const brandGreen = Color(0xFF3ABD83);
+  static const brandRed = Color(0xFFC95241);
+  static const brandPurple = Color(0xFF774DDD);
+
+  static const card1Stroke = Color(0xFF2F2D40);
+  static const card2Stroke = Color(0xFF353348);
+
+  static const heading1Text = Color(0xFFFFFFFF);
+  static const heading2Text = Color(0xFFD9DBE5);
+  static const paragraph1Text = Color(0xFFBDBCC2);
+  static const paragraph2Text = Color(0xFF7C7A86);
+  static const paragraph3Text = Color(0xFF434152);
+
+  static const buttonPrimaryText = Color(0xFFFFFFFF);
+  static const buttonSecondaryText = Color(0xFFFFFFFF);
+  static const blackText = Color(0xFF1D1B30);
+  static const whiteText = Color(0xFFFFFFFF);
+
+  static const linkActive = Color(0xFFFECE96);
+
+  static const systemError = Color(0xFFEC8171);
+  static const systemInfo = Color(0xFF85C4E1);
+  static const systemSuccess = Color(0xFF8AC0A8);
+  static const systemWarning = Color(0xFFE9BF8F);
+  static const elementDivider = Color(0xFF282639);
+
+  static const caretColor = Color(0xFF574F92);
+
+  static const overlayBackground = Color(0xFF0D0C14);
+}
