a1efe0d55177aee5664cca47a1c73fde0e542d52
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-07-09 22:00:57 +0400

    feat(features): Update default ui of homepage.

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_amplify_auth_starter/theme/dark_colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,9 +7,17 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
+      backgroundColor: DarkColors.backgroundBody,
       body: const Center(
-        child: Text('Welcome to the Home Page!'),
+        child: Text(
+          'Welcome to the Home Page!',
+          style: TextStyle(
+            color: DarkColors.heading1Text,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.5,
+          ),
+        ),
       ),
     );
   }
