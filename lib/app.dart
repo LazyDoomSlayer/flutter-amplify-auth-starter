2296dbb153c51422d23e823e795834fb1cd700db
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-07-09 22:00:29 +0400

    feat(root): Configure auth middleware for redirection to homepage on authed user.

--- lib/app.dart
+++ lib/app.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_amplify_auth_starter/middlewares/auth_gate.dart';
+
 import 'core/constants.dart';
 import 'features/auth/login/login_page.dart';
 import 'features/auth/reset_password/reset_password_page.dart';
 import 'features/home/home_page.dart';
 
 class App extends StatelessWidget {
-  const App({super.key});
+  final String initialRoute;
+
+  const App({super.key, required this.initialRoute});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Amplify Auth',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.blue,
-      ),
-      initialRoute: AppRoutes.login,
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue),
+      home: const AuthGate(),
       routes: {
         AppRoutes.login: (context) => const LoginPage(),
         AppRoutes.resetPassword: (context) => const ResetPasswordPage(),
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
+// main.dart
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_amplify_auth_starter/core/constants.dart';
 
 import 'amplify_config.dart';
 import 'app.dart';
 
+Future<bool> isUserSignedIn() async {
+  final res = await Amplify.Auth.fetchAuthSession();
+  return res.isSignedIn;
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await configureAmplify(); // your Amplify setup
+  bool signedIn = await isUserSignedIn();
 
-  await configureAmplify();
-
-  runApp(const App());
+  runApp(App(initialRoute: signedIn ? AppRoutes.home : AppRoutes.login));
 }
