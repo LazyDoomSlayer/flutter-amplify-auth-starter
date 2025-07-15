ccc1c86e02c730cc311d72d67105a61b86da7931
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-07-15 22:17:51 +0400

    feat(features): Add local location gathering.

--- lib/features/home/map_page.dart
+++ lib/features/home/map_page.dart
@@ -1,6 +1,8 @@
 import 'package:arcgis_maps/arcgis_maps.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_amplify_auth_starter/constants/map_scale_levels.dart';
 import 'package:flutter_amplify_auth_starter/theme/dark_colors.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -12,17 +14,53 @@ class MapPage extends StatefulWidget {
 class _MapPageState extends State<MapPage> {
   final _mapViewController = ArcGISMapView.createController();
 
-  void onMapViewReady() {
-    final map = ArcGISMap.withBasemapStyle(BasemapStyle.arcGISTopographic);
+  Future<Position> getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
 
-    _mapViewController.arcGISMap = map;
-    _mapViewController.setViewpoint(
-      Viewpoint.withLatLongScale(
-        latitude: 34.02700,
-        longitude: -118.80500,
-        scale: 72000,
-      ),
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      throw Exception('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        throw Exception('Location permission denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      throw Exception('Location permissions are permanently denied.');
+    }
+
+    final LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.bestForNavigation,
+      distanceFilter: 100,
     );
+
+    return await await Geolocator.getCurrentPosition(
+      locationSettings: locationSettings,
+    );
+  }
+
+  void onMapViewReady() async {
+    final map = ArcGISMap.withBasemapStyle(BasemapStyle.arcGISTopographic);
+    try {
+      final position = await getCurrentLocation();
+
+      _mapViewController.arcGISMap = map;
+      _mapViewController.setViewpoint(
+        Viewpoint.withLatLongScale(
+          latitude: position.latitude,
+          longitude: position.longitude,
+          scale: MapScaleLevels.neighborhood,
+        ),
+      );
+    } catch (e) {
+      print('Location error: $e');
+    }
   }
 
   @override
