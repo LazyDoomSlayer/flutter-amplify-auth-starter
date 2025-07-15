59692687373c58a2b37273a54e523dbb15b57deb
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-07-15 22:17:34 +0400

    feat(constants): Add map scale constants.

---
+++ lib/constants/map_scale_levels.dart
@@ -0,0 +1,10 @@
+class MapScaleLevels {
+  static const double neighborhood = 5_000;
+  static const double community = 20_000;
+  static const double urban = 50_000;
+  static const double district = 250_000;
+  static const double region = 800_000;
+  static const double nation = 4_000_000;
+  static const double nationPlus = 10_000_000;
+  static const double continent = 20_000_000;
+}
