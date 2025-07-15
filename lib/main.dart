4c42e4a198677a5b0c82c8b6a1fa058961a25422
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-07-15 21:46:03 +0400

    feat(lib): Add esri map key init.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 // main.dart
 import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:arcgis_maps/arcgis_maps.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_amplify_auth_starter/core/constants.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'amplify_config.dart';
 import 'app.dart';
@@ -12,8 +14,18 @@ Future<bool> isUserSignedIn() async {
 }
 
 void main() async {
+  await dotenv.load(fileName: ".env");
+
+  final apiKey = dotenv.env['ESRI_API_KEY'];
+
+  if (apiKey == null || apiKey.isEmpty) {
+    throw Exception('ESRI_API_KEY is not defined in .env file.');
+  } else {
+    ArcGISEnvironment.apiKey = apiKey;
+  }
+
   WidgetsFlutterBinding.ensureInitialized();
-  await configureAmplify(); // your Amplify setup
+  await configureAmplify();
   bool signedIn = await isUserSignedIn();
 
   runApp(App(initialRoute: signedIn ? AppRoutes.home : AppRoutes.login));
