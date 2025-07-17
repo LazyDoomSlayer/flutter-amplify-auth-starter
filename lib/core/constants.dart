5584efe58c60b3961c84782070d6596fa15fa711
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-07-17 23:35:45 +0400

    feat(core): Add routes for new flows.

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,5 +1,7 @@
 class AppRoutes {
   static const login = '/login';
   static const resetPassword = '/reset-password';
+  static const setNewPassword = '/set-new-password';
+  static const newPasswordSetted = '/new-password-setted';
   static const home = '/home';
 }
