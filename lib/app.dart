c96211a50070f86561d1f3440707b79909cd1250
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-07-19 20:17:46 +0400

    fix(root): Fix issues related to loading of app states.

--- lib/app.dart
+++ lib/app.dart
@@ -2,16 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter_amplify_auth_starter/features/auth/reset_password/new_password_setted/new_password_setted_page.dart';
 import 'package:flutter_amplify_auth_starter/features/auth/reset_password/set_new_password/set_new_password_page.dart';
 import 'package:flutter_amplify_auth_starter/middlewares/auth_gate.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'core/constants.dart';
 import 'features/auth/login/login_page.dart';
 import 'features/auth/reset_password/reset_password_page.dart';
 import 'features/home/home_page.dart';
 
-class App extends StatelessWidget {
-  final String initialRoute;
+class App extends StatefulWidget {
+  const App({super.key});
 
-  const App({super.key, required this.initialRoute});
+  @override
+  State<App> createState() => _App();
+}
+
+class _App extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+    initialisation();
+  }
+
+  void initialisation() async {
+    await Future.delayed(const Duration(seconds: 1));
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,15 @@
-// main.dart
-import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:arcgis_maps/arcgis_maps.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_amplify_auth_starter/core/constants.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'amplify_config.dart';
 import 'app.dart';
 
-Future<bool> isUserSignedIn() async {
-  final res = await Amplify.Auth.fetchAuthSession();
-  return res.isSignedIn;
-}
-
 void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   await dotenv.load(fileName: ".env");
 
   final apiKey = dotenv.env['ESRI_API_KEY'];
@@ -24,9 +20,7 @@ void main() async {
     ArcGISEnvironment.apiKey = apiKey;
   }
 
-  WidgetsFlutterBinding.ensureInitialized();
   await configureAmplify();
-  bool signedIn = await isUserSignedIn();
 
-  runApp(App(initialRoute: signedIn ? AppRoutes.home : AppRoutes.login));
+  runApp(const App());
 }
